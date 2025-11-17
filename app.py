9649d1621b348f1098b93be826768c2a8ad855e5
yukiyew <167262231+071210@users.noreply.github.com>
2025-11-17 21:42:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -107,6 +107,4 @@ def redirect_html():
     return redirect(url_for(route_name))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)
-
+    app.run(host="0.0.0.0", port=8080)
